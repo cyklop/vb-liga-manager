f8f50a7d7ad175e229d59506d356f1445283e183
Steve Schneider <steve.schneider@empiriecom.com>
2025-04-16 14:37:53 +0200

    stagged changes

--- config/auth.ts
+++
@@ -1,4 +0,0 @@
-export const SUPERADMIN_CREDENTIALS = {
-  username: 'superadmin',
-  password: 'superadmin123'
-}
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // swcMinify is default and the option is deprecated/removed in newer Next.js versions
-}
-
-module.exports = nextConfig
